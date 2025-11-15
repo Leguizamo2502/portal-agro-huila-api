4646e23dae20db15e450372200779597fd9050db
Sergio <vargasleguizamo95@gmail.com>
2025-11-14 22:12:56 -0500

    feat(ConsumerRating):Se añadio la funcionalidad para que le productor pueda calificar al cunsimidor despues de completar un pedido

---
+++ Portal-Agro-comercial-del-Huila/Entity/Validation/Validations/Orders/ConsumerRatings/ConsumerRatingCreateDtoValidator.cs
@@ -0,0 +1,36 @@
+﻿using Entity.DTOs.Order.ConsumerRatings.Create;
+using FluentValidation;
+
+namespace Entity.Validation.Validations.Orders.ConsumerRatings
+{
+    public class ConsumerRatingCreateDtoValidator : AbstractValidator<ConsumerRatingCreateDto>
+    {
+        public ConsumerRatingCreateDtoValidator()
+        {
+            RuleFor(x => x.Rating)
+                .InclusiveBetween((byte)1, (byte)5);
+
+            RuleFor(x => x.Comment)
+                .MaximumLength(500)
+                .When(x => !string.IsNullOrWhiteSpace(x.Comment));
+
+            RuleFor(x => x.RowVersion)
+                .NotEmpty()
+                .Must(IsBase64).WithMessage("RowVersion inválido.");
+        }
+
+        private static bool IsBase64(string? value)
+        {
+            if (string.IsNullOrWhiteSpace(value)) return false;
+            try
+            {
+                Convert.FromBase64String(value);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
