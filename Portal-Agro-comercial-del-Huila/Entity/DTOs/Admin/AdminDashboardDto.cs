b754f64e328b8c53dcc401000c67e3a20c3a4518
Sergio <vargasleguizamo95@gmail.com>
2025-11-18 14:41:18 -0500

    feat(dasboard): Endpoints para creacion de dashboard

---
+++ Portal-Agro-comercial-del-Huila/Entity/DTOs/Admin/AdminDashboardDto.cs
@@ -0,0 +1,13 @@
+﻿using Entity.DTOs.Producer.Analytics;
+
+namespace Entity.DTOs.Admin
+{
+    public class AdminDashboardDto
+    {
+        public OrderFunnelDto OrderFunnel { get; set; } = new();
+        public PaymentSummaryDto Payments { get; set; } = new();
+        public CatalogSummaryDto Catalog { get; set; } = new();
+        public IEnumerable<TopProducerStatDto> TopProducers { get; set; } = Array.Empty<TopProducerStatDto>();
+        public IEnumerable<TopProductStatDto> TopProducts { get; set; } = Array.Empty<TopProductStatDto>();
+    }
+}
