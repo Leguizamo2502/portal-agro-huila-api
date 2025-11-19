2fd4d32e87a6cbc1f0d0b0aed18e23d22a20b834
Sergio <vargasleguizamo95@gmail.com>
2025-11-19 10:21:40 -0500

    feat(chatOrder): Version preliminar de char

---
+++ Portal-Agro-comercial-del-Huila/Entity/DTOs/Orders/OrderChat/OrderChatMessagesPageDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Entity.DTOs.Orders.OrderChat
+{
+    public class OrderChatMessagesPageDto
+    {
+        public int OrderId { get; set; }
+        public string OrderCode { get; set; } = string.Empty;
+        public int ConversationId { get; set; }
+        public int Total { get; set; }
+        public bool HasMore { get; set; }
+        public IReadOnlyCollection<OrderChatMessageDto> Messages { get; set; } = Array.Empty<OrderChatMessageDto>();
+    }
+}
